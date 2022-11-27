9991a558d0002d82f5993f694968b55e0a5e56ef
Metehan Metinoğlu <mete3777@gmail.com>
2022-11-28 02:58:47 +0300

    First

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+
+namespace Array_List;
+class Program
+{
+    static void Main(string[] args)
+    {
+        ArrayList liste=new ArrayList();
+        //liste.Add("Metehan");
+        liste.Add(2);
+        //liste.Add(true);
+        //liste.Add('M');
+
+        //Verilere erişim
+        Console.WriteLine(liste[0]);
+
+        foreach (var item in liste)Console.WriteLine(item);
+
+        //AddRange
+        Console.WriteLine("****AddRange");
+        string[] renkler={"Mavi","Kırmızı","Turkuaz"};
+        List<int> sayilar = new List<int>(){2,5,7,4,15};
+
+        //liste.AddRange(renkler);
+        liste.AddRange(sayilar);
+
+        foreach (var item in liste)
+        Console.WriteLine(item);
+
+        //Sort
+        Console.WriteLine("********* Sort **********");
+        liste.Sort();
+
+        foreach (var item in liste)
+        Console.WriteLine(item);
+
+        //binary search
+        Console.WriteLine("****Binary Search*********");
+        Console.WriteLine(liste.BinarySearch(7));
+
+        //reverse
+        Console.WriteLine("****Reverse*****");
+        liste.Reverse();
+        foreach (var item in liste)
+        Console.WriteLine(item);
+
+        //Clear
+        liste.Clear();
+
+        foreach (var item in liste)
+        Console.WriteLine(item);
+
+    }
+}
